679508a72f46941f41955296c2e8d9a17bdff120
sling254 <mwanikidenis7@gmail.com>
2022-06-12 23:06:31 +0300

    Add jwt settings

--- apis/api/views.py
+++ apis/api/views.py
@@ -1,6 +1,12 @@
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
+@api_view(['GET'])
 def getRouter(request):
     routes =  [
         '/api/token',
@@ -8,5 +14,5 @@ def getRouter(request):
     ]
 
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
 
